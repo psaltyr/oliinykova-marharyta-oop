9543c04b629d4c71d7a423c68f1b26b5be75c0c8
Oliinykova Marharyta <blleeedddnnnaya@gmail.com>
2024-10-26 22:56:09 +0300

    - Switched checks to get Sulfuras from the outside.

--- lab3/src/com/gildedrose/GildedRose.java
+++ lab3/src/com/gildedrose/GildedRose.java
@@ -46,8 +46,8 @@ class GildedRose {
             }
         }
         else {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
             }

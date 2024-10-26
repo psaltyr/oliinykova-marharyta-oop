8956f1f05bf6668582e3c31d4002f4b525fdcc39
Oliinykova Marharyta <blleeedddnnnaya@gmail.com>
2024-10-26 22:32:12 +0300

    - Extract the processExpired() method.

--- lab3/src/com/gildedrose/GildedRose.java
+++ lab3/src/com/gildedrose/GildedRose.java
@@ -18,20 +18,24 @@ class GildedRose {
         updateExpiration(item);
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+            processExpired(item);
+        }
+    }
+
+    private static void processExpired(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality-- ;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality-- ;
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
